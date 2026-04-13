f570caf6de822586b075a1d85b69b12cd3f06795
Anzai <55052716+owari-hash@users.noreply.github.com>
2026-04-13 23:06:36 +0800

    Update version to 1.1.10 in package.json and package-lock.json; add 'use client' directive to Chatbot and Livechat components

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useMemo, useState } from 'react';
 import { ChatbotSchema } from '../schemas';
 import { bgMap } from '../engine/Tokens';
--- src/components/Livechat.tsx
+++ src/components/Livechat.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useMemo, useState } from 'react';
 import { LivechatSchema } from '../schemas';
 import { bgMap } from '../engine/Tokens';
